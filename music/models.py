b91e7d7c615ab1a37070f55a6d984b42622f5df5
123Pedro <pedro.curi20@gmail.com>
2017-05-22 13:02:16 -0500

    creacion de model

--- music/models.py
+++ music/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import  models
 
-# Create your models here.
+class Album(models.model):
+    artist=models.CharField(max_length=250)
+    album_title=models.CharField(max_length=500)
+    genro=models.CharField(max_length=100)
+    album_logo=models.CharField(max_length=1000)
+
+class Song(models.model):
+   album=models.ForeignKey(Album,on_delete=models.CASCADE)
+    file_type=models.CharField(max_length=20)
+    song_title=models.CharField(max_length=250)
